92a372f0024d929a6ffc02c0bdd3c64391b079d8
Hoang Tien <hoangtrungtien99@gmail.com>
2022-03-29 12:44:54 +0700

    ?

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let input = document.querySelector("input")
+
 async function _hanlderApi() {
     try {
         const res = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2022-02-28&sortBy=publishedAt&apiKey=1f5210480a3d48d487a6e90bd780eadc")
@@ -12,10 +13,15 @@ async function _hanlderApi() {
             contentNode.innerHTML = `<div class="col-6"><a href="${test[i].url}" target="_blank"><img class="rounded" src="${test[i].urlToImage}"/></a></div>
                                     <div class="col-6 ms-3"><h1 class="fs-3 fw-bold">${test[i].title}</h1>
                                     <p class="fs-5">${test[i].description}</p></div>`
-            content.appendChild(contentNode)
+            _clear(content)
+                                    content.appendChild(contentNode)
         }
+        
     } catch(e) {
         alert("Loi")
     }
 }
 
+        const _clear = (e) => {
+            console.log(e)
+        }
